8c8d09c459684aedf197892791e8eb8a630bb9da
milan-stefanik <stefanik.milan@gmail.com>
2020-02-11 14:40:24 +0000

    JS reference

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//code copied from "https://stackoverflow.com/questions/55360887/navbar-not-functioning-like-its-supposed-to"
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar");
