83645da04f61da23a113830fc7c69a4c8ba88343
milan-stefanik <stefanik.milan@gmail.com>
2020-02-11 10:47:35 +0000

    Scrollspy Expertise Layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
 });
 
