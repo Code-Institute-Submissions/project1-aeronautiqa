135395d880fb7960bde4f41008d549bcb81f4a43
milan-stefanik <59015822+milan-stefanik@users.noreply.github.com>
2020-02-13 11:07:11 +0300

    "script adjustment"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //code copied from "https://stackoverflow.com/questions/55360887/navbar-not-functioning-like-its-supposed-to" and adjusted
 $(function () {
-    $(document).scroll(function () {
+    $(window).pageYOffset(function () {
         var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > 100);
+        $nav.toggleClass('scrolled', $(window).pageYOffset > 100);
     });
 });
